0f5e1728a09336da3abdebabbf59c3a79a687c52
Ranjit Das <ranjitdasajodhyapur38@gmail.com>
2025-08-31 00:17:20 +0530

    Fix error handling in asyncHandler to use correct error object and status code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -19,9 +19,9 @@ const asyncHandler = (fn) => async (req, res, next) => {
     try {
         await fn(req, res, next)
     } catch (error) {
-        res.status(err.code || 500).json({
+        res.status(error.code || 400).json({
             success: false,
-            message: err.message
+            message: error.message
         })
     }
 }
